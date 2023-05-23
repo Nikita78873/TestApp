c15f2d83ca469a9c75f3a84355f13a20391bf785
Nikita78873 <buev.nik@yandex.ru>
2023-05-23 20:32:54 +0800

    321

--- flutter_application_1/lib/pages/instructionpage.dart
+++ flutter_application_1/lib/pages/instructionpage.dart
@@ -10,10 +10,57 @@ class InstructionPage extends StatefulWidget {
 class _InstructionPageState extends State<InstructionPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text(
-        'hello'
-      ),
+    return Scaffold(
+      body: Column(children: <Widget>[
+        Padding(
+          padding: const EdgeInsetsDirectional.fromSTEB(0, 30, 0, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Expanded(
+                flex: 9,
+                child: Text(
+                  'Решение',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                )
+              ),
+              Expanded(
+                flex: 1,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    minimumSize: const Size(20, 40),
+                  ),
+                  child: const Icon(
+                    Icons.settings_rounded,
+                    size: 24,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const InstructionPage()),
+                    );
+                  }
+                )
+              )
+            ]
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: const [
+              Flexible(
+                child: Text(
+                  'dsd'
+                ),
+              )
+            ],
+          )
+        )  
+      ]),
     );
   }
 }
